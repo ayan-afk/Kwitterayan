bb7461d9695f162a8fd57439c1361e77718476c5
ayan-afk <79571638+ayan-afk@users.noreply.github.com>
2021-07-23 16:39:31 +0530

    Update kwitter_page.js

--- kwitter/kwitter_page.js
+++ kwitter/kwitter_page.js
@@ -1,13 +1,14 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyD7tncD3an944l2SjiwEeuhB6Yw0BNswPM",
-  authDomain: "kwitter-a104f.firebaseapp.com",
-  projectId: "kwitter-a104f",
-  storageBucket: "kwitter-a104f.appspot.com",
-  messagingSenderId: "743534563806",
-  appId: "1:743534563806:web:ca79456b9e0245c8240d4f"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyC7rvhJUPj-96m387rh8220wAPX1kpvqaw",
+    authDomain: "jsdbtest.firebaseapp.com",
+    databaseURL: "https://jsdbtest.firebaseio.com",
+    projectId: "jsdbtest",
+    storageBucket: "jsdbtest.appspot.com",
+    messagingSenderId: "557097620929",
+    appId: "1:557097620929:web:d549209de61a48b20b8b40"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 	user_name = localStorage.getItem("user_name");
 	room_name = localStorage.getItem("room_name");
 
